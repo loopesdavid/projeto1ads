b1cf106af90f5c4c5a8fe551375ca476c40122f2
david <loopesdavid@gmail.com>
2025-08-20 17:35:35 -0300

    Duplicando o botao de sair

--- testeste1/frmMenu.Designer.cs
+++ testeste1/frmMenu.Designer.cs
@@ -32,6 +32,7 @@
             this.btnSair = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblMenu
@@ -75,11 +76,22 @@
             this.button2.Text = "Cadastro";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(101, 329);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(163, 54);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Sair";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // frmMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(365, 381);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnSair);
@@ -96,5 +108,6 @@
         private System.Windows.Forms.Button btnSair;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
